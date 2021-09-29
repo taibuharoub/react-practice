795e5d1bc7b2e83d71f6bbf8e819f2bd529505c7
Taibu Haroub <haroubtaibu@gmail.com>
2021-09-29 16:14:30 +0300

    Manage the Error state

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -8,13 +8,23 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsernmae] = useState("");
   const [enteredAge, setEnteredage] = useState("");
+  //initial value is undefined, so no need to pass any initial value
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid Input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid Age",
+        message: "Please enter a valid age (> 0).",
+      });
       return;
     }
 
@@ -31,10 +41,20 @@ const AddUser = (props) => {
   const ageChangeHandler = (event) => {
     setEnteredage(event.target.value);
   };
+  const errorHandler = () => {
+    // setting the error back to undefined, for closing the modal
+    setError(null);
+  };
 
   return (
     <div>
-      <ErrorModal title="An Error ocurred!" message="Something went wrong!" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
